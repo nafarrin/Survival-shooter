5e2963f13520e2105132a613391350535fd57dc9
GustavoACasanNunez <115280027+GustavoACasanNunez@users.noreply.github.com>
2023-11-08 15:50:24 +0100

    initial kick of

--- Assets/_CompletedAssets/MobileInput/Scripts/Editor/CrossPlatformInputInitialize.cs
+++ Assets/_CompletedAssets/MobileInput/Scripts/Editor/CrossPlatformInputInitialize.cs
@@ -84,7 +84,7 @@ namespace UnitySampleAssets.CrossPlatformInput.Inspector
         private static BuildTargetGroup[] buildTargetGroups = new BuildTargetGroup[]
             {
                 BuildTargetGroup.Standalone,
-                BuildTargetGroup.WebGL,
+                BuildTargetGroup.WebPlayer,
                 BuildTargetGroup.Android,
                 BuildTargetGroup.iOS,
                 BuildTargetGroup.WP8,
